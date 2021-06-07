d373e761e8b5c384b6c5be914a2d560196b807e0
Kreshnik Ramadani <44235462+KreshnikRamadani@users.noreply.github.com>
2021-06-07 20:49:46 +0200

    Analyzing the "Housing" data set & plotting histogram of numeric attributes.

--- part-one/chapter2/housing.py
+++ part-one/chapter2/housing.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # let’s load the data using pandas
 def load_housing_data(housing_path="datasets"):
@@ -8,3 +9,17 @@ def load_housing_data(housing_path="datasets"):
 
 housing_data = load_housing_data()
 print(housing_data.head())
+
+# get a quick description of the data: total number of rows, attribute’s type, number of nonnull values
+print(housing_data.info())
+
+# There are 10 attributes = 9 Numeric + 1 categorical 
+# what categories exist and how many districts are there
+print(housing_data["ocean_proximity"].value_counts())
+
+# get summary of each numerical attribute
+print(housing_data.describe())
+
+# plot a histogram for each numerical attribute
+housing_data.hist(bins=50, figsize=(20,15))
+plt.show()
