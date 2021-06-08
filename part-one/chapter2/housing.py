328427e211db64d5d8b578284836a551ad90ee78
Kreshnik Ramadani <44235462+KreshnikRamadani@users.noreply.github.com>
2021-06-08 20:30:48 +0200

    Getting insights of data via histogram

--- part-one/chapter2/housing.py
+++ part-one/chapter2/housing.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from zlib import crc32
 
 # let’s load the data using pandas
 def load_housing_data(housing_path="datasets"):
@@ -23,3 +25,26 @@ print(housing_data.describe())
 # plot a histogram for each numerical attribute
 housing_data.hist(bins=50, figsize=(20,15))
 plt.show()
+
+# METHOD 1: split the data into training set and testing set
+def split_train_test(data, test_ratio):
+    np.random.seed(42)
+    shulffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data)*test_ratio)
+    test_indices = shulffled_indices[:test_set_size]
+    train_indices = shulffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+#METHOD 2: split the data into training set and testing set
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio *2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda x: test_set_check(x, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+train_set, test_set = split_train_test(housing_data, 0.2)
+
+print('Train set: ', len(train_set) )
+print('Test set: ', test_set.head())
